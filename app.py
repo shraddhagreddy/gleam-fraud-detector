71f475d65c1d6776fe5b99515f19e94ac81cf670
shraddhagreddy <shraddhagreddy@gmail.com>
2025-09-21 18:00:01 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -280,4 +280,5 @@ def reset_db():
     return redirect(url_for("appeals_page"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
